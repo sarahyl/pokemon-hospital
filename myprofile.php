e47711485ae416cce7d08eee382b44fe339770cb
sarahyl <sarah.liu.va@gmail.com>
2024-01-11 21:00:24 -0500

    initial

---
+++ myprofile.php
@@ -0,0 +1,152 @@
+<?php
+session_start();
+require("connect-db.php");
+require("pokemon-db.php");
+require("nurse-db.php");
+
+// error_reporting(0);
+
+#if not logged in, redirect to login page
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: login.php');
+	exit;
+}
+
+$is_charge_nurse = isChargeNurse($_SESSION['user']['nurse_ID']);
+$profile_info = getNurseProfileInfo($_SESSION['user']['nurse_ID']);
+$phone_numbers = getNursePhoneNumbers($_SESSION['user']['nurse_ID']);
+$specialities = getNurseSpecialties($_SESSION['user']['nurse_ID']);
+$assigned_pokemon = getAssignedPokemon($_SESSION['user']['nurse_ID']);
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+   if (!empty($_POST['deleteBtn']))
+   {
+    deletePokemonByID($_POST['pokemonID_to_delete']);
+    $assigned_pokemon = getAssignedPokemon($_SESSION['user']['nurse_ID']);
+   }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+
+    <body>
+    <!-- navbar -->
+    <ul>
+        <li><a class="active" href="pokemonform.php">Home</a></li>
+        <li><a href="myprofile.php">My Profile</a></li>
+        <li><a href="add-patient.php">Add Patient</a></li>
+        <li><a href="patient-search.php">Patient Search</a></li>
+        <?php if ($is_charge_nurse[0]) : ?>
+            <li><a href="nursesearch.php">Nurse Search</a></li>
+        <?php endif; ?>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+
+
+    
+    <div class="container">
+        <div style="text-align: center;">
+        <img src="photos/myprofile.png" height="300">
+        </div>
+        <br>
+        <h3> My Profile </h3>
+        <p>First Name: <?php echo $profile_info['name_first']; ?> </p>
+        <p>Last Name: <?php echo $profile_info['name_last']; ?> </p>
+        <p>Nurse ID: <?php echo $profile_info['nurse_ID']; ?> </p>
+        <p>Phone number(s):
+            <?php
+            //if the nurse has more than 1 number
+            if (is_array($phone_numbers[0])) {
+                $phone_array = array();
+                foreach ($phone_numbers as $phone_number):
+                    array_push($phone_array, $phone_number[0]);
+                endforeach;
+                echo implode(",", $phone_array);
+            } else { // if the nurse only has one phone number
+                echo $phone_numbers[0];
+            }
+            ?>
+
+        </p>
+        <p>Specialties:
+        <?php
+            //if the nurse has more than 1 specialty
+            if (is_array($specialities[0])) {
+                $specialty_array = array();
+                foreach ($specialities as $specialty):
+                    array_push($specialty_array, $specialty[0]);
+                endforeach;
+                echo implode(", ", $specialty_array);
+            } else { // if the nurse only has one specialty
+                echo $specialities[0];
+            }
+        ?>
+        </p>
+    </div>
+    <br>
+    <div class="container">
+        <h3>My Patients</h3>
+        <div class="row ">
+        <table class="w3-table w3-bordered w3-card-4 center" style="width:70%">
+        <thead>
+        <tr style="background-color:#B0B0B0">
+
+            <!--<th width="30%">Name-->
+            <th>ID
+            <th>Name
+            <th>Weight
+            <th>Type
+            <th>Date of Birth
+            <th>Last Visit
+            <th>Insurance
+            <th>&nbsp;</th>
+            <th>&nbsp;</th>
+        </tr>
+        </thead>
+
+        <?php foreach ($assigned_pokemon as $pokemon): ?>
+        <tr>
+            <td><?php echo $pokemon['pokemon_ID']; ?></td>
+            <td><?php echo $pokemon['name']; ?></td>
+            <td><?php echo $pokemon['weight']; ?></td>
+            <td><?php echo $pokemon['type']; ?></td>
+            <td><?php echo $pokemon['date_of_birth']; ?></td>
+            <td><?php echo $pokemon['last_visit']; ?></td>
+            <td><?php echo $pokemon['insurance']; ?></td>
+            <td>
+
+                <form action="pokemonform.php" method="post">
+                <input type="hidden" name="pokemon_ID_to_update" value="<?php echo $pokemon['pokemon_ID']; ?>"/>
+                <input type="hidden" name="pokemon_name_to_update" value="<?php echo $pokemon['name']; ?>"/>
+                <input type="hidden" name="weight_to_update" value="<?php echo $pokemon['weight']; ?>"/>
+                <input type="hidden" name="type_to_update" value="<?php echo $pokemon['type']; ?>"/>
+                <input type="hidden" name="date_of_birth_to_update" value="<?php echo $pokemon['date_of_birth']; ?>"/>
+                <input type="hidden" name="last_visit_to_update" value="<?php echo $pokemon['last_visit']; ?>"/>
+                <input type="hidden" name="insurance_to_update" value="<?php echo $pokemon['insurance']; ?>"/>
+                </form>
+            <td>
+            
+            
+            <form action="myprofile.php" method="post">
+                <input type="submit" value="Delete" name="deleteBtn" class="btn btn-danger"/>
+                <input type="hidden" name="pokemonID_to_delete" value="<?php echo $pokemon['pokemon_ID']; ?>"/>
+            </form>
+            
+            </td>
+        </tr>
+        <?php endforeach; ?>
+
+        </table>
+        </div>
+    </div>
+    </body>
+</html>
---
+++ pokemon-db.php
@@ -0,0 +1,295 @@
+<?php
+function addPokemon($name, $weight, $type, $date_of_birth, $last_visit, $insurance, $trainer_ID, $nurse_ID)
+{
+  global $db;
+
+  $query = "insert into pokemon (name, weight, type, date_of_birth, last_visit, insurance, trainer_ID, nurse_ID) values (:name, :weight, :type, :date_of_birth, :last_visit, :insurance, :trainer_ID, :nurse_ID) ";
+  // prepare:
+  // 1. prepare (compile)
+  // 2. bindValue + exe
+
+  $statement = $db->prepare($query);
+  $statement->bindValue(':name', $name);
+  $statement->bindValue(':weight', $weight);
+  $statement->bindValue(':type', $type);
+  $statement->bindValue(':date_of_birth', $date_of_birth);
+  $statement->bindValue(':last_visit', $last_visit);
+  $statement->bindValue(':insurance', $insurance);
+  $statement->bindValue(':trainer_ID', $trainer_ID);
+  $statement->bindValue(':nurse_ID', $nurse_ID);
+  $statement->execute();
+  $statement->closeCursor();
+}
+
+function getAllPokemon()
+{
+  global $db;
+  $query = "select * from pokemon";
+  $statement = $db->prepare($query);
+  $statement->execute();
+  $results = $statement->fetchAll();   // fetch()
+  $statement->closeCursor();
+  return $results;
+}
+
+function getAssignedPokemon($nurse_ID){
+  global $db;
+  $query = "select * from pokemon where nurse_ID='".$nurse_ID."'";
+  $statement = $db->prepare($query);
+  $statement->execute();
+  $results = $statement->fetchAll();   // fetch()
+  $statement->closeCursor();
+  return $results;
+}
+
+function getPokemonInfo($pokemon_ID){
+  global $db;
+
+  $query = "SELECT pokemon.*, GROUP_CONCAT(DISTINCT pokemon_allergies.allergy) as allergies,
+  GROUP_CONCAT(DISTINCT pokemon_illnesses.illness) as illnesses,
+  GROUP_CONCAT(DISTINCT pokemon_injuries.injury) as injuries,
+  GROUP_CONCAT(DISTINCT pokemon_medications.medication) as medications
+  FROM pokemon
+  LEFT JOIN pokemon_allergies ON pokemon.pokemon_ID = pokemon_allergies.pokemon_ID
+  LEFT JOIN pokemon_illnesses ON pokemon.pokemon_ID = pokemon_illnesses.pokemon_ID
+  LEFT JOIN pokemon_injuries ON pokemon.pokemon_ID = pokemon_injuries.pokemon_ID
+  LEFT JOIN pokemon_medications ON pokemon.pokemon_ID = pokemon_medications.pokemon_ID
+  WHERE pokemon.pokemon_ID=:pokemon_ID
+  GROUP BY pokemon.pokemon_ID";
+
+  $statement = $db->prepare($query);
+
+  // Bind the parameter before executing the statement
+  $statement->bindParam(':pokemon_ID', $pokemon_ID);
+
+  $statement->execute();
+  $results = $statement->fetchAll();
+  $statement->closeCursor();
+  return $results;
+
+}
+
+function getTrainerInfo($trainer_ID){
+  global $db;
+
+  $query = "SELECT trainer.*, trainer_phone.phone_number
+  FROM trainer
+  LEFT JOIN trainer_phone ON trainer.trainer_ID = trainer_phone.trainer_ID
+  WHERE trainer.trainer_ID=:trainer_ID";
+
+  $statement = $db->prepare($query);
+
+  // Bind the parameter before executing the statement
+  $statement->bindParam(':trainer_ID', $trainer_ID);
+
+  $statement->execute();
+  $results = $statement->fetchAll();
+  $statement->closeCursor();
+  return $results;
+}
+
+function updatePokemonByID($pokemon_ID, $name, $weight, $type, $date_of_birth, $last_visit, $insurance)
+{
+  global $db;
+  $query = "update pokemon set name=:name, weight=:weight, type=:type, date_of_birth=:date_of_birth, last_visit=:last_visit, insurance=:insurance where pokemon_ID=:pokemon_ID";
+
+  $statement = $db->prepare($query);
+  $statement->bindValue(':name', $name);
+  $statement->bindValue(':weight', $weight);
+  $statement->bindValue(':type', $type);
+  $statement->bindValue(':date_of_birth', $date_of_birth);
+  $statement->bindValue(':last_visit', $last_visit);
+  $statement->bindValue(':pokemon_ID', $pokemon_ID); // Add this line to bind the ID
+  $statement->bindValue(':insurance', $insurance);
+  $statement->execute();
+  $statement->closeCursor();
+}
+
+function deletePokemonByID($pokemon_ID)
+{
+  global $db;
+  $query = "delete from pokemon where pokemon_ID=:pokemon_ID";
+  $statement = $db->prepare($query);
+  $statement->bindValue(':pokemon_ID', $pokemon_ID);
+  $statement->execute();
+  $statement->closeCursor();
+
+}
+
+
+function getNurseProfileInfo($nurse_ID)
+{
+  global $db;
+  $query = "select * from nurse where nurse_ID=$nurse_ID";
+  $statement = $db->prepare($query);
+  $statement->execute();
+  $results = $statement->fetch();   // fetch()
+  $statement->closeCursor();
+  return $results;
+}
+
+function getNursePhoneNumbers($nurse_ID)
+{
+  global $db;
+  $query = "select phone_number from nurse_phone where nurse_ID=$nurse_ID";
+  $statement = $db->prepare($query);
+  $statement->execute();
+  $results = $statement->fetch();   // fetch()
+  $statement->closeCursor();
+  return $results;
+}
+
+function getNurseSpecialties($nurse_ID)
+{
+  global $db;
+  $query = "select specialty from nurse_specialties where nurse_ID=$nurse_ID";
+  $statement = $db->prepare($query);
+  $statement->execute();
+  $results = $statement->fetchAll();   // fetch()
+  $statement->closeCursor();
+  return $results;
+}
+
+function searchPokemon($name)
+{
+  global $db;
+  $query = "select * from pokemon where name='".$name."'";
+  $statement = $db->prepare($query);
+  $statement->execute();
+  $results = $statement->fetchAll();   // fetch()
+  $statement->closeCursor();
+  return $results;
+}
+
+function getPokemonByID($pokemon_ID) 
+{
+  global $db;
+  $query = "select * from pokemon where pokemon_ID=:pokemon_ID";
+  $statement = $db->prepare($query);
+  $statement->bindValue(':pokemon_ID', $pokemon_ID);
+  $statement->execute();
+  $result = $statement->fetchAll(PDO::FETCH_ASSOC); 
+  $statement->closeCursor();
+
+  return $result;
+}
+
+function getIllnessesByID($pokemon_ID)
+{
+  global $db;
+  $query = "select * from pokemon_illnesses where pokemon_ID=:pokemon_ID";
+  $statement = $db->prepare($query);
+  $statement->bindValue(':pokemon_ID', $pokemon_ID);
+  $statement->execute();
+  $result = $statement->fetchAll(PDO::FETCH_ASSOC); 
+  $statement->closeCursor();
+
+  return $result;
+}
+
+function getInjuriesByID($pokemon_ID)
+{
+  global $db;
+  $query = "select * from pokemon_injuries where pokemon_ID=:pokemon_ID";
+  $statement = $db->prepare($query);
+  $statement->bindValue(':pokemon_ID', $pokemon_ID);
+  $statement->execute();
+  $result = $statement->fetchAll(PDO::FETCH_ASSOC); 
+  $statement->closeCursor();
+
+  return $result;
+}
+
+function getAllergiesByID($pokemon_ID)
+{
+  global $db;
+  $query = "select * from pokemon_allergies where pokemon_ID=:pokemon_ID";
+  $statement = $db->prepare($query);
+  $statement->bindValue(':pokemon_ID', $pokemon_ID);
+  $statement->execute();
+  $result = $statement->fetchAll(PDO::FETCH_ASSOC); 
+  $statement->closeCursor();
+
+  return $result;
+}
+
+function getMedicationsByID($pokemon_ID)
+{
+  global $db;
+  $query = "select * from pokemon_medications where pokemon_ID=:pokemon_ID";
+  $statement = $db->prepare($query);
+  $statement->bindValue(':pokemon_ID', $pokemon_ID);
+  $statement->execute();
+  $result = $statement->fetchAll(PDO::FETCH_ASSOC); 
+  $statement->closeCursor();
+
+  return $result;
+}
+
+function exportHealthRecords($pokemon_ID, $pokemon_name)
+{
+  global $db;
+
+  header('Content-Type: text/csv');
+  header('Content-Disposition: attachment; filename="' . $pokemon_name . '-health-records.csv"');
+
+  $output = fopen('php://output', 'w');
+
+  $pokemon_info = getPokemonByID($pokemon_ID);
+  $pokemon_illnesses = getIllnessesByID($pokemon_ID);
+  $pokemon_injuries = getInjuriesByID($pokemon_ID);
+  $pokemon_allergies = getAllergiesByID($pokemon_ID);
+  $pokemon_medications = getMedicationsByID($pokemon_ID);
+
+  $infoHeaders = array('pokemon_ID', 'nurse_ID', 'trainer_ID', 'name', 'weight', 'type', 'date_of_birth','last_visit','insurance');
+  $illnessHeaders = array('pokemon_ID', 'illness');
+  $injuriesHeaders = array('pokemon_ID', 'injury');
+  $allergiesHeaders = array('pokemon_ID', 'allergy');
+  $medicationsHeaders = array('pokemon_ID', 'medication');
+
+  fputcsv($output, $infoHeaders);
+  foreach ($pokemon_info as $data) {
+      fputcsv($output, $data);
+  }
+  fputcsv($output, array());
+
+  fputcsv($output, $illnessHeaders);;
+  foreach ($pokemon_illnesses as $illness) {
+    fputcsv($output, $illness);
+  }
+  fputcsv($output, array());
+
+  fputcsv($output, $injuriesHeaders);
+  foreach ($pokemon_injuries as $injury) {
+    fputcsv($output, $injury);
+  }
+  fputcsv($output, array());
+
+  fputcsv($output, $allergiesHeaders);
+  foreach ($pokemon_allergies as $allergy) {
+    fputcsv($output, $allergy);
+  }
+  fputcsv($output, array());
+
+  fputcsv($output, $medicationsHeaders);
+  foreach ($pokemon_medications as $medication) {
+    fputcsv($output, $medication);
+  }
+
+    fclose($output);
+  exit();
+}
+
+
+function getAllTrainers()
+{
+  global $db;
+  $query = "select * from trainer";
+  $statement = $db->prepare($query);
+  $statement->execute();
+  $results = $statement->fetchAll();   // fetch()
+  $statement->closeCursor();
+  return $results;
+}
+
+?>
